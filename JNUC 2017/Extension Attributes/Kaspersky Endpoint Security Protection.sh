46f55c71174f922f10fb574e5a0d38cf7c9f36d8
Scott Blake <mitchelsblake@gmail.com>
2018-02-09 00:56:04 -0500

    Update Kaspersky Endpoint Security Protection.sh

--- JNUC 2017/Extension Attributes/Kaspersky Endpoint Security Protection.sh
+++ JNUC 2017/Extension Attributes/Kaspersky Endpoint Security Protection.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RESULT=$(timeout 2 /usr/local/bin/kav status Protection | awk '/Protection/ {print $2}')
+RESULT=$(/usr/local/bin/kav status Protection | awk '/Protection/ {print $2}')
 
 if [ "${RESULT}" = "" ]; then
-    echo "<result>$(timeout 2 /usr/local/bin/kav status)</result>"
+    echo "<result>$(/usr/local/bin/kav status)</result>"
 else
     echo "<result>${RESULT}</result>"
 fi
